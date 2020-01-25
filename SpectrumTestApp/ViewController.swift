d463b8da42872f0e398be209ea16a0b17f2ab8b9
MacBookPro <shaandealsmobiledev@gmail.com>
2020-01-25 12:15:36 +0530

    Initial Commit

---
+++ SpectrumTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpectrumTestApp
+//
+//  Created by MacBookPro on 1/25/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
